d10469b1b00c9ef71aa02ea5c6cb8e90e1afb52c
John Ellee Robado <bachrobado@gmail.com>
2020-10-15 03:38:23 +0800

    first commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function About() {
+    return (
+        <React.Fragment>
+            <div style={{ textAlign:"center" }}>
+                <h1>About</h1>
+                <p>This is Todo App. It is part of React Crash Course Tutorial By Traversy Media</p>
+            </div>
+        </React.Fragment>
+    )
+}
+
+export default About;
